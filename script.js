b1194de026d621bae714cbb39bcaf7afeb532d44
Hamim hossain <hamimhossainrocks@gmail.com>
2021-02-11 21:52:50 +0600

    not finished yet

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const searchMeal = () =>{
+const searchMeals = () =>{
     const searchText = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/${searchText}`;
-    console.log(url);
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=` + searchText
+    // load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container');
+
+    meals.forEach(meal => {
+        const li = document.createElement('li');
+        li.innerText = meal.strMeal;
+        mealContainer.appendChild(li);
+
+    });
 }
